36d065b70dbd4f5612399a89a35c111c1c85cdbe
Christian <munhiz.christian@gmail.com>
2021-05-23 13:04:33 +0200

    Ejercicio 4 completado

---
+++ PEC6_Ej4/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {WineitemComponent} from './components/wines/wineitem/wineitem.component';
+import {WinelistComponent} from './components/wines/winelist/winelist.component';
+import {WineNewComponent} from './components/wines/wine-new/wine-new.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WineitemComponent,
+    WinelistComponent,
+    WineNewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [HttpClient],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
